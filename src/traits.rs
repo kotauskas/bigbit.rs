2bd9bb4a5048a29515bab1de9dd05e56c51d9a55
Kotauskas <v.toncharov@gmail.com>
2020-06-25 18:06:25 +0300

    Sealed the traits

--- src/traits.rs
+++ src/traits.rs
@@ -3,7 +3,9 @@
 /// The usual process for getting both the division result and the remainder is performing both operations sequentially, since most programming languages, including Rust, do not provide an interface for introspecting the remainder acquired after dividing with the designated CPU instructions (LLVM will, however, optimize the two operations into one, given that optimizations are actually enabled). With BitBit numbers, however, division is implemented iteratively, without harware acceleration, i.e. the CPU doesn't assist the process by providing special instructions to perform the operations.
 ///
 /// The current division implementation simply drops the remainder, which has the cost of deallocating the coefficient/`LinkedByte` sequence, only to allocate and calculate it yet again. By using this trait, you ensure that nothing is ever dropped during division and that you get both the quotient and remainder the fastest way possible.
-pub trait DivRem<Rhs = Self>: Sized {
+///
+/// This trait is **sealed**, i.e. cannot be implemented for types outside of the `bigbit` crate, which allows adding new methods to the trait without breaking changes and prevents logic errors.
+pub trait DivRem<Rhs = Self>: Sealed {
     /// The type for the quotient.
     type Quotient;
     /// The type for the remainder.
@@ -17,8 +19,10 @@ pub trait DivRem<Rhs = Self>: Sized {
 ///
 /// This serves the same purpose as the [`DivRem`] trait, but the division is performed in place (the variable/field containing the dividend is replaced by the quotient), and the only value returned is the remainder.
 ///
+/// This trait is **sealed**, i.e. cannot be implemented for types outside of the `bigbit` crate, which allows adding new methods to the trait without breaking changes and prevents logic errors.
+///
 /// [`DivRem`]: trait.DivRem.html "DivRem — combined division and remainder operations"
-pub trait DivRemAssign<Rhs = Self> {
+pub trait DivRemAssign<Rhs = Self>: Sealed {
     /// The type for the remainder.
     type Remainder;
     /// Performs combined quotient and remainder calculation, returning the remainder and setting the left operand to the quotient.
@@ -31,9 +35,8 @@ pub trait DivRemAssign<Rhs = Self> {
 ///
 /// Until specialization becomes stable, not implementing this trait transitively is a logic error rather than a scenario which is protected against by a default blanket implementation. **In short, if you implement `Gcd<U>` for type `T`, you need to also implement `Gcd<T>` for `U`. It's a viable option to do that by writing an `#[inline(always)]` shim which calls `T::gcd(value_of_u)`.
 ///
-/// ## This is not a general-purpose trait!
-/// **Implementing this trait for types outside of the `bigbit` crate is greatly discouraged for semantic purposes.** Use non-trait methods or third-party traits instead.
-pub trait Gcd<Rhs = Self>: Sized {
+/// This trait is **sealed**, i.e. cannot be implemented for types outside of the `bigbit` crate, which allows adding new methods to the trait without breaking changes and prevents logic errors.
+pub trait Gcd<Rhs = Self>: Sealed {
     /// Performs the calculation of the greatest common divisor.
     ///
     /// Most implementations use the [Euclidean algorithm][0] for this.
@@ -50,11 +53,10 @@ pub trait Gcd<Rhs = Self>: Sized {
 ///
 /// For an in-place version, see [`AddAssignAt`].
 ///
-/// ## This is not a general-purpose trait!
-/// **Implementing this trait for types outside of the `bigbit` crate is greatly discouraged for semantic purposes.** Use non-trait methods or third-party traits instead.
+/// This trait is **sealed**, i.e. cannot be implemented for types outside of the `bigbit` crate, which allows adding new methods to the trait without breaking changes and prevents logic errors.
 ///
 /// [`AddAssignAt`]: trait.AddAssignAt.html "AddAssignAt — performs the += operation at the specified coefficient byte of a BigBit number"
-pub trait AddAt<Rhs = Self> {
+pub trait AddAt<Rhs = Self>: Sealed {
     /// The return type for the operation.
     type Output;
     /// Performs the shifting addition.
@@ -69,11 +71,35 @@ pub trait AddAt<Rhs = Self> {
 ///
 /// For a version which returns the result instead of performing the operation in-place, see [`AddAt`].
 ///
-/// # This is not a general-purpose trait!
-/// **Implementing this trait for types outside of the `bigbit` crate is greatly discouraged for semantic purposes.** Use non-trait methods or third-party traits instead.
+/// This trait is **sealed**, i.e. cannot be implemented for types outside of the `bigbit` crate, which allows adding new methods to the trait without breaking changes and prevents logic errors.
 ///
 /// [`AddAt`]: trait.AddAt.html "AddAt — performs the + operation at the specified coefficient byte of a BigBit number"
-pub trait AddAssignAt<Rhs = Self> {
+pub trait AddAssignAt<Rhs = Self>: Sealed {
     /// Performs the shifting addition in-place.
     fn add_assign_at(&mut self, index: usize, rhs: Rhs);
+}
+
+use seal::Sealed;
+mod seal {
+    use crate::{
+        linkedbytes::*,
+        headbyte::*,
+    };
+
+    /// Disallows outside implementations for the traits, allowing for breaking changes to those traits in minor/patch releases.
+    pub trait Sealed {}
+    macro_rules! allow_sealed_for {
+        ($($ty:ty)+) => ($(
+            impl Sealed for $ty {}
+        )+)
+    }
+
+    allow_sealed_for! {
+        LBNum
+        LBNumRef<'_>
+        LinkedByte
+        
+        HBNum
+        HeadByte
+    }
 }
